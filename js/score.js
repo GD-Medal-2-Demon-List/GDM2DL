9f4ce736175f2c7a208a8ab38fb05bcd2d3a1509
GDM2 <gdmedaltwo@gmail.com>
2026-01-22 00:21:16 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    if (rank > 100) {
         return 0;
     }
     if (rank > 75 && percent < 100) {
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
     */
     // New formula
     let score = (-24.9975*Math.pow(rank-1, 0.5) + 250) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+        ((percent - (minPercent - 1)) / (150 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
